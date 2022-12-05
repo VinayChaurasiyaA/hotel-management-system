1bfcc6710c1df087d3c3c579e1af98619f258bf5
VINAY CHAURASIYA <92705023+VinayChaurasiyaA@users.noreply.github.com>
2022-12-05 23:14:27 +0530

    Update create.sql

--- psql/create.sql
+++ psql/create.sql
@@ -10,7 +10,7 @@ DROP TABLE IF EXISTS HotelManager;
 DROP TABLE IF EXISTS Consumer;
 DROP TABLE IF EXISTS "User";
 
-create table "User" (
+create table User (
 	id			serial,
 	name		text,
 	address		text,
@@ -24,7 +24,7 @@ create table HotelOwner (
 	userName	text NOT NULL,
 	password	text NOT NULL,
 	PRIMARY KEY (id), 
-	FOREIGN KEY (id) REFERENCES "User"(id),
+	FOREIGN KEY (id) REFERENCES User(id),
 	CONSTRAINT userNameOwner_integrity UNIQUE(userName)
 );
 
@@ -33,14 +33,14 @@ create table HotelManager (
 	userName	text NOT NULL,
 	password	text NOT NULL,
 	PRIMARY KEY (id), 
-	FOREIGN KEY (id) REFERENCES "User"(id) ,
+	FOREIGN KEY (id) REFERENCES User(id) ,
 	CONSTRAINT userNameManager_integrity UNIQUE(userName)
 );
 
 create table Consumer (
 	id			INTEGER,
 	PRIMARY KEY (id), 
-	FOREIGN KEY (id) REFERENCES "User"(id)
+	FOREIGN KEY (id) REFERENCES User(id)
 );
 
 create table Hotel (
@@ -60,7 +60,7 @@ create table HotelUnavailability (
 	hotelID		INTEGER NOT NULL,
 	status		text NOT NULL,
 	start		DATE NOT NULL,
-	"end"		DATE NOT NULL,
+	end		DATE NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY	(hotelID) REFERENCES Hotel(id) 
 );
@@ -77,11 +77,11 @@ create table HotelRoom (
 	id serial,
 	hotelID		INTEGER,
 	roomNum		INTEGER,
-	"type"		text NOT NULL,
+	type		text NOT NULL,
 	status		text NOT NULL DEFAULT 'AVAILABLE',
 	PRIMARY KEY	(id),
 	FOREIGN KEY (hotelID) REFERENCES Hotel(id),
-	FOREIGN KEY ("type") REFERENCES RoomType(id),
+	FOREIGN KEY (type) REFERENCES RoomType(id),
 	UNIQUE (hotelID,roomNum)
 );
 
@@ -90,7 +90,7 @@ create table DiscountRate (
 	hotelID		INTEGER NOT NULL,
 	roomType	TEXT NOT NULL,
 	start		DATE NOT NULL,
-	"end"		DATE NOT NULL,
+	end		DATE NOT NULL,
 	discRate	decimal NOT NULL,
 	PRIMARY KEY	(id),
 	FOREIGN KEY (hotelID) REFERENCES Hotel(id),
@@ -102,7 +102,7 @@ create table Booking (
 	hotelID		INTEGER NOT NULL,
 	consumerID	INTEGER NOT NULL,
 	start		DATE NOT NULL,
-	"end"		DATE NOT NULL,
+	end		DATE NOT NULL,
 	PIN			VARCHAR(10) NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY	(hotelID) REFERENCES Hotel(id),
@@ -118,7 +118,7 @@ create table BookingRecord (
 	room		INTEGER DEFAULT NULL REFERENCES HotelRoom(id),
 	rmnumber INTEGER DEFAULT NULL,
 	start date not null,
-	"end" date not null,
+	end date not null,
 	price		decimal NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY (bookingID) REFERENCES Booking(id),
@@ -126,13 +126,13 @@ create table BookingRecord (
 	FOREIGN KEY (hotelID) REFERENCES Hotel(id)
 );
 
-insert into "User" (name,address,phoneNum,email) values ('John','1 Anzac Pde., NSW 2035',040111111,'john@gmail.com');
+insert into User (name,address,phoneNum,email) values ('John','1 Anzac Pde., NSW 2035',040111111,'john@gmail.com');
 insert into HotelOwner values (1,'john','password');
 
-insert into "User" (name,address,phoneNum,email) values ('Mary','2 Anzac Pde., NSW 2035',0401111112,'mary@gmail.com');
+insert into User (name,address,phoneNum,email) values ('Mary','2 Anzac Pde., NSW 2035',0401111112,'mary@gmail.com');
 insert into HotelManager values (2,'mary','password');
 
-insert into "User" (name,address,phoneNum,email) values ('Eric','3 Anzac Pde., NSW 2035',040111113,'eric@gmail.com');
+insert into User (name,address,phoneNum,email) values ('Eric','3 Anzac Pde., NSW 2035',040111113,'eric@gmail.com');
 insert into HotelManager values (3,'eric','password');
 
 insert into Hotel (name,managerID,ownerID,address,phone) values ('Elizabeth Branch',2,1,'1 Elizabeth St., NSW 2000',040111114);
@@ -144,16 +144,16 @@ insert into RoomType values ('Queen (1 double bed)',2,true,160);
 insert into RoomType values ('Executive (1 double bed, more facilities than Queen)',2,true,200);
 insert into RoomType values ('Suite (2 double beds, most luxurious)',4,true,400);
 
-insert into HotelRoom (hotelID,roomNum,"type") values (1,1,'Single Room (with 1 single bed)');
-insert into HotelRoom (hotelID,roomNum,"type") values (1,2,'Twin Bed (2 single beds)');
-insert into HotelRoom (hotelID,roomNum,"type") values (1,3,'Twin Bed (2 single beds)');
-insert into HotelRoom (hotelID,roomNum,"type") values (1,4,'Queen (1 double bed)');
-insert into HotelRoom (hotelID,roomNum,"type") values (1,5,'Executive (1 double bed, more facilities than Queen)');
-insert into HotelRoom (hotelID,roomNum,"type") values (1,6,'Suite (2 double beds, most luxurious)');
+insert into HotelRoom (hotelID,roomNum,type) values (1,1,'Single Room (with 1 single bed)');
+insert into HotelRoom (hotelID,roomNum,type) values (1,2,'Twin Bed (2 single beds)');
+insert into HotelRoom (hotelID,roomNum,type) values (1,3,'Twin Bed (2 single beds)');
+insert into HotelRoom (hotelID,roomNum,type) values (1,4,'Queen (1 double bed)');
+insert into HotelRoom (hotelID,roomNum,type) values (1,5,'Executive (1 double bed, more facilities than Queen)');
+insert into HotelRoom (hotelID,roomNum,type) values (1,6,'Suite (2 double beds, most luxurious)');
 
-insert into HotelRoom (hotelID,roomNum,"type") values (2,1,'Single Room (with 1 single bed)');
-insert into HotelRoom (hotelID,roomNum,"type") values (2,2,'Twin Bed (2 single beds)');
-insert into HotelRoom (hotelID,roomNum,"type") values (2,3,'Twin Bed (2 single beds)');
-insert into HotelRoom (hotelID,roomNum,"type") values (2,4,'Queen (1 double bed)');
-insert into HotelRoom (hotelID,roomNum,"type") values (2,5,'Executive (1 double bed, more facilities than Queen)');
-insert into HotelRoom (hotelID,roomNum,"type") values (2,6,'Suite (2 double beds, most luxurious)');
+insert into HotelRoom (hotelID,roomNum,type) values (2,1,'Single Room (with 1 single bed)');
+insert into HotelRoom (hotelID,roomNum,type) values (2,2,'Twin Bed (2 single beds)');
+insert into HotelRoom (hotelID,roomNum,type) values (2,3,'Twin Bed (2 single beds)');
+insert into HotelRoom (hotelID,roomNum,type) values (2,4,'Queen (1 double bed)');
+insert into HotelRoom (hotelID,roomNum,type) values (2,5,'Executive (1 double bed, more facilities than Queen)');
+insert into HotelRoom (hotelID,roomNum,type) values (2,6,'Suite (2 double beds, most luxurious)');
